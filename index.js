0b014473b3641d43a6f3acb753df627818b4b6f2
ProgrammerBayzid <mdbayzid2622002@gmail.com>
2022-11-09 11:42:56 +0600

    jwt commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const jwt = require('jsonwebtoken')
 
 require('dotenv').config()
 const app = express()
@@ -21,11 +21,48 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).send({
+            message: 'unauthorized access'
+        })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            res.status(403).send({
+                message: 'unauthorized access'
+            })
+        }
+        req.decoded = decoded;
+        next()
+    })
+};
+
+
+
+
+
+
 async function run() {
     try {
         const servicesCollection = client.db('photography').collection('services');
         const oderCollection = client.db('photography').collection('orders');
         const reviewCollection = client.db('photography').collection('reviews');
+
+
+
+        // jwt Sicrecet token
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+
+
 
 
 
@@ -58,7 +95,9 @@ async function run() {
 
 
         // reviews api
-        app.get('/reviews', async (req, res) => {
+        app.get('/reviews', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log('insude order api', decoded);
             let query = {};
             if (req.query.email) {
                 query = {
@@ -78,7 +117,9 @@ async function run() {
         });
 
 
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log('insude order api', decoded);
             let query = {};
             if (req.query.email) {
                 query = {
