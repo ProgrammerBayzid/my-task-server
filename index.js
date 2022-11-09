eeb0527d3650d813e7e2092f1c9a07e501c5d3a8
ProgrammerBayzid <mdbayzid2622002@gmail.com>
2022-11-09 17:11:34 +0600

    token set

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run() {
 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '10' })
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '10s' })
             res.send({ token })
         })
 
